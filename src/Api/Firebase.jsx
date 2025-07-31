d4131b6dfab9549a2f96811a2a575823bbe53ab6
jasmine-taneja <jasmine.taneja07@gmail.com>
2025-07-31 21:44:25 +0530

    fix: exposed Api Keys

--- src/Api/Firebase.jsx
+++ src/Api/Firebase.jsx
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD_kCUVq-JshZsma60qL3MXePQdTkYKKDI",
-  authDomain: "kodomo-firebase.firebaseapp.com",
-  projectId: "kodomo-firebase",
-  storageBucket: "kodomo-firebase.firebasestorage.app",
-  messagingSenderId: "1090167464457",
-  appId: "1:1090167464457:web:f63d3e9d7b6f38cf068ed8",
-  measurementId: "G-HC0M55B3HQ"
+  apiKey: import.meta.apiKey,
+  authDomain: import.meta.authDomain,
+  projectId: import.meta.projectId,
+  storageBucket: import.meta.storageBucket,
+  messagingSenderId: import.meta.apiKeymessagingSenderId,
+  appId: import.meta.appId,
+  measurementId: import.meta.measurementId
 };
 
 const app = initializeApp(firebaseConfig);
